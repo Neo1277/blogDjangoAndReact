5cefeeeb4634f3272ac93d3fe898101fe0fdd61d
Marlon Meneses <marlon-armando@hotmail.com>
2021-02-12 01:33:54 -0500

    Creating unit test for genre API endpoint

--- backend_django/posts/tests.py
+++ backend_django/posts/tests.py
@@ -1,3 +1,55 @@
-from django.test import TestCase
+from django.urls import include, path, reverse
+from rest_framework.test import APITestCase, URLPatternsTestCase
+from rest_framework import status
+from .models import Genre, Post
 
-# Create your tests here.
+def create_genre(genre_data):
+    """
+    Create a genre with the given `genre_data` dictionary
+    """
+
+    return Genre.objects.create(**genre_data)
+
+def create_post(post_data):
+    """
+    Create a post with the given `post_data` dictionary
+    """
+
+    return Post.objects.create(**post_data)
+
+class AccountTests(APITestCase):
+
+    def test_genres_get_endpoint(self):
+        """
+        Ensure we can create a new genre object.
+        """
+        genre_data = {
+            'name': 'Movies',
+            'slug':'movies',
+            'description':   'description of Movies',
+            'show_menu_list':'YES'
+        }
+
+        genre = create_genre(genre_data)
+
+        """
+        post_data = {
+            'title': 'Spiderman 3',
+            'slug':'spiderman',
+            'description':   'description of spiderman',
+            'status': 1,
+            'country':'US',
+            'image_post':'posts/2f474f0e-1168-4f00-b123-2556c30cf385.jpg',
+            'author_id': 1
+        }
+
+        create_post(post_data)
+        """
+
+        url = reverse('posts:genres')
+
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+# https://www.django-rest-framework.org/api-guide/testing/#example_1
+# https://docs.djangoproject.com/en/3.1/intro/tutorial05/#testing-the-detailview
--- backend_django/posts/urls.py
+++ backend_django/posts/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from django.conf.urls import url 
 from . import views
 
-#Set end point for requests
+# Set end point for requests
+app_name = 'posts'
 urlpatterns = [
-    url(r'api/genres$', views.GenresListView.as_view()),
+    url(r'api/genres$', views.GenresListView.as_view(), name='genres'),
     url(r'api/posts$', views.PostsListView.as_view()),
     url(r'api/featured_posts$', views.FeaturedPostsListView.as_view()),
     url(r'api/comments$', views.CommentsView.as_view()),
